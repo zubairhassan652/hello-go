005b4c70464856e92b56505ed96422a3b9cfdffd
Zubair Hassan <zubairhassan@Mac-sSE-Zubair-146.local>
2021-07-15 16:33:27 +0500

    fixed

--- hello.go
+++ hello.go
@@ -1,23 +1,38 @@
 package main
 
 import (
-"github.com/gin-gonic/contrib/static"
-"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 
 	r := gin.Default()
-	r.GET("/hello", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, From InvoZone!")
 	})
 	api := r.Group("/api")
-	api.GET("/ping", func(c *gin.Context) {
+	api.GET("/detail", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"country_name": "pakistan",
+			"company_name": "invozone",
+			"company_type": "provides technology services around the glob",
+			"website": "invozone.com",
 		})
 	})
-	r.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	//r.LoadHTMLGlob("templates/*")
+	r.LoadHTMLFiles("views/hello.html")
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "hello.html", gin.H{
+			"title": "Main website",
+		})
+	})
+
+
+	r.Static("/assets", "./views/css")
+	r.StaticFS("/static", http.Dir("./views"))
+	//r.Use(static.Serve("/static", static.LocalFile("./views", true)))
 	r.Run()
 }
 
